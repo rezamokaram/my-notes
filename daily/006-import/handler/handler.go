4c67b5ff2c3b54e3c15adaad1f9640879076dc83
Reza Mokaram <rezamokaramdost@gmail.com>
2025-01-08 22:06:59 +0330

    new notes

--- daily/006-import/handler/handler.go
+++
@@ -1,5 +0,0 @@
-package handler
-
-func PrintFromHandler() {
-	println("Hello from handler")
-}
--- daily/006-import/service/service.go
+++
@@ -1,8 +0,0 @@
-package service
-
-import "main/handler"
-
-func PrintFromService() {
-	println("Hello from service")
-	handler.PrintFromHandler()
-}
